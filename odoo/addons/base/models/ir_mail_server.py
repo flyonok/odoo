abc4d43870d1327ec697c3acca0c58940e2d7d1a
flyonok@163.com <flyonok@163.com>
2020-09-30 09:56:17 +0800

    test push

--- odoo/addons/base/models/ir_mail_server.py
+++ odoo/addons/base/models/ir_mail_server.py
@@ -397,7 +397,8 @@ class IrMailServer(models.Model):
 
         '''
         get_param = self.env['ir.config_parameter'].sudo().get_param
-        postmaster = get_param('mail.bounce.alias', default='postmaster-odoo')
+        # postmaster = get_param('mail.bounce.alias', default='postmaster-odoo')
+        postmaster = get_param('mail.bounce.alias', default='254755020')
         domain = get_param('mail.catchall.domain')
         if postmaster and domain:
             _logger.info('%s@%s--xiaowy' % (postmaster, domain))

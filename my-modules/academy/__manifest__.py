373e5eb6b91fa903c8ce174b1c981d804a223800
flyonok@163.com <flyonok@163.com>
2020-10-16 14:01:13 +0800

    modified academy __mainfest__

--- my-modules/academy/__manifest__.py
+++ my-modules/academy/__manifest__.py
@@ -11,7 +11,7 @@
     """,
 
     'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'website': "http://www.y0y1.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -21,6 +21,8 @@
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
+    'installable': True,
+    'application': True,
 
     # always loaded
     'data': [

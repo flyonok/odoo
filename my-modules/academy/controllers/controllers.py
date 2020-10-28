3cb4e0f949c06f328f3622ae77ace35c63756ba2
flyonok@163.com <flyonok@163.com>
2020-10-28 17:35:01 +0800

    add simple route teacher

--- my-modules/academy/controllers/controllers.py
+++ my-modules/academy/controllers/controllers.py
@@ -12,9 +12,14 @@ class Academy(http.Controller):
         # return http.request.render('academy.index', {
         #     'teachers': ["Diana Padilla", "Jody Caroll", "Lester Vaughn"],
         # })
-    @http.route('/academy/<int:id>/', auth='public', website=True)
-    def teacher(self, id):
-        return '<h1>{} ({})</h1>'.format(id, type(id).__name__)
+    # @http.route('/academy/<int:id>/', auth='public', website=True)
+    # def teacher(self, id):
+    #     return '<h1>{} ({})</h1>'.format(id, type(id).__name__)
+    @http.route('/academy/<model("academy.teachers"):teacher>/', auth='public', website=True)
+    def teacher(self, teacher):
+        return http.request.render('academy.biography', {
+            'person': teacher
+        })
 
 #     @http.route('/academy/academy/objects/', auth='public')
 #     def list(self, **kw):

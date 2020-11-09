447c593c3162208fa5acfd1ddfd7bb3c05b865b2
flyonok@163.com <flyonok@163.com>
2020-11-09 13:07:25 +0800

    TZ=>UTC+8

--- odoo/netsvc.py
+++ odoo/netsvc.py
@@ -128,7 +128,7 @@ def init_logger():
     logging.addLevelName(25, "INFO")
     logging.captureWarnings(True)
     # add by xiaowy 2020/11/09
-    logging.Formatter.converter = time.localtime
+    logging.Formatter.converter = time.gmtime
 
     from .tools.translate import resetlocale
     resetlocale()
